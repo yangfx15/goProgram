2bdd7c274a7828ddd598502359e1dc8f3fb0f8cf
yangfx15 <40289509+yangfx15@users.noreply.github.com>
2020-11-06 17:41:36 +0800

    Create fetchall.go

---
+++ fetchall.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+// 返回数据大小，url，及响应时间
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+	resp.Body.Close()
+	ch <- fmt.Sprintf("%.2f %d %s", time.Since(start).Seconds(), nbytes, url)
+}
+
+func main()  {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
